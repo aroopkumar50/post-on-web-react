57c617e4302fad0bb36a748d9c1b0cd4cb7a0a22
Aroop Kumar <aroopkumar50@gmail.com>
2023-01-30 07:00:49 +0530

    Add Post component and css module for it

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Post from "./components/Post";
+
 function App() {
-  return <h1>Hello World!</h1>;
+    return (
+        <main>
+            <Post author="Aroop" body="Message1" />
+            <Post author="Kumar" body="Message2"/>
+            <Post />
+        </main>
+    );
 }
 
 export default App;
